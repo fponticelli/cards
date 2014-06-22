75a631c001f82189b9628dc6b8755f55ef90498e
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-06-22 18:39:32 +0200

    added enabled state for buttons

--- haxe/src/ui/Button.hx
+++ haxe/src/ui/Button.hx
@@ -2,12 +2,15 @@ package ui;
 
 import js.html.Event;
 import steamer.Producer;
+import steamer.Value;
 import sui.components.Component;
 using steamer.dom.Dom;
+using steamer.Consumer;
 
 class Button {
 	public var component(default, null) : Component;
 	public var clicks(default, null) : Producer<Event>;
+	public var enabled(default, null) : Value<Bool>;
 	var cancel : Void -> Void;
 	public function new(text = '', ?icon : String) {
 		component = new Component({
@@ -18,6 +21,14 @@ class Button {
 		var pair = component.el.produceEvent('click');
 		clicks = pair.producer;
 		cancel = pair.cancel;
+
+		enabled = new Value(true);
+		enabled.feed(function(value : Bool) {
+			if(value)
+				component.el.removeAttribute("disabled");
+			else
+				component.el.setAttribute("disabled", "disabled");
+		}.toConsumer());
 	}
 
 	public function destroy() {
