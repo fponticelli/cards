3794d9dfbe7343355675caee5a35e4bf152fea45
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-12 23:14:32 -0600

    improved TextEditor initialization speed

--- src/cards/ui/input/TextEditor.hx
+++ src/cards/ui/input/TextEditor.hx
@@ -9,6 +9,12 @@ using thx.stream.dom.Dom;
 
 // TODO focus
 class TextEditor extends Editor {
+  function resize() {
+    var el = component.el;
+    el.style.height = "5px";
+    el.style.height = (1+el.scrollHeight) + "px";
+  }
+
   public function new(container : Element) {
     var options = {
       template  : '<textarea class="editor text" placeholder="type text"></textarea>',
@@ -19,11 +25,12 @@ class TextEditor extends Editor {
     var el : TextAreaElement = cast component.el;
     el.style.content = 'type text';
     el.streamEvent('input')
-      .audit(function(_) {
-        el.style.height = "5px";
-        el.style.height = (1+el.scrollHeight) + "px";
-      })
+      .audit(function(_) resize())
       .mapValue(function(_) return (el.value : TypedValue))
       .plug(stream);
+    el.streamFocus().feed(focus);
+
+    // TODO, this considerably slow downs the creation of many items (like in array)
+    //resize();
   }
 }
