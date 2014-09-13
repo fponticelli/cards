25c5b3dc663c2c375743354d0f3aaf675a9adb8d
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-12 23:16:58 -0600

    minor doc

--- src/cards/ui/input/EditorFactory.hx
+++ src/cards/ui/input/EditorFactory.hx
@@ -4,6 +4,8 @@ import js.html.Element;
 import cards.components.Component;
 import cards.model.SchemaType;
 
+
+// TODO editors are not picking up parent
 class EditorFactory {
   public static function create(type : SchemaType, container : Element, parent : Component) : IEditor {
     return switch type {
