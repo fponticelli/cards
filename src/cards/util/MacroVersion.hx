45a3e0def4d43772414ffc0a314c38c505277406
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-18 21:05:52 -0600

    updated to use semver

--- src/cards/util/MacroVersion.hx
+++ src/cards/util/MacroVersion.hx
@@ -7,6 +7,7 @@ package cards.util;
 #if macro
 import haxe.macro.Context;
 import haxe.macro.Expr;
+import thx.semver.Version;
 #end
 class MacroVersion {
   macro public static function currentVersion()
@@ -14,27 +15,32 @@ class MacroVersion {
 
   macro public static function next()
 #if major
-    return saveAndReturn(getVersion().incrementMajor());
+    return saveAndReturn(getVersion().nextMajor());
 #elseif minor
-    return saveAndReturn(getVersion().incrementMinor());
+    return saveAndReturn(getVersion().nextMinor());
 #elseif (patch || release)
-    return saveAndReturn(getVersion().incrementPatch());
+    return saveAndReturn(getVersion().nextPatch());
+#elseif pre
+    return saveAndReturn(getVersion().nextPre());
 #else
-    return saveAndReturn(getVersion().incrementBuild());
+    return saveAndReturn(getVersion().nextBuild());
 #end
 
 
   macro public static function nextBuild()
-    return saveAndReturn(getVersion().incrementBuild());
+    return saveAndReturn(getVersion().nextBuild());
+
+  macro public static function nextPre()
+    return saveAndReturn(getVersion().nextPre());
 
   macro public static function nextPatch()
-    return saveAndReturn(getVersion().incrementPatch());
+    return saveAndReturn(getVersion().nextPatch());
 
   macro public static function nextMinor()
-    return saveAndReturn(getVersion().incrementMinor());
+    return saveAndReturn(getVersion().nextMinor());
 
   macro public static function nextMajor()
-    return saveAndReturn(getVersion().incrementMajor());
+    return saveAndReturn(getVersion().nextMajor());
 
   macro public static function setVersionFile(file : String) {
     VERSION_FILE = file;
@@ -43,11 +49,10 @@ class MacroVersion {
 
 #if macro
   static var VERSION_FILE = "version";
-  static function getVersion() : Version {
+  static function getVersion() : Version
     return !sys.FileSystem.exists(VERSION_FILE)
-        ? new Version(0, 0, 0, 0)
-        : Version.fromString(sys.io.File.getContent(VERSION_FILE));
-  }
+        ? ([0,0,0] : Version)
+        : (sys.io.File.getContent(VERSION_FILE) : Version);
 
   static function saveAndReturn(version : Version) {
     saveVersion(version);
@@ -56,13 +61,13 @@ class MacroVersion {
 
   static function saveVersion(version : Version) {
     var file = sys.io.File.write(VERSION_FILE, false);
-    file.writeString(version.fullVersion());
+    file.writeString(version.toString());
     file.close();
   }
 
   static function expr(version : Version) {
     return {
-      expr : ExprDef.EConst(Constant.CString(version.fullVersion())),
+      expr : ExprDef.EConst(Constant.CString(version.toString())),
       pos : Context.currentPos()
     };
   }
